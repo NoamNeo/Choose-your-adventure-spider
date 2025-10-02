051496c2ecf39888e55a621e53053dbd19489007
GabiL <gabriel.loira.lorenzo@gmail.com>
2025-10-02 12:16:24 +0200

    Mejoramos el proyecto según las indicaciones de Sergio

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,22 @@ public class Main{
         return(numInput);
     }
     public static void main (String[] Args){
+        int decionesTomadas =0;
         int decisionJuego;
+        boolean happyEnding = false;
+        boolean trueEnding = false;
+        boolean crossoverEnding = false;
+        boolean cowardEnding = false;
+        boolean globalDomination = false;
         Scanner userInputVar = new Scanner(System.in);
         String nombre;
         System.out.println("Eres una araña, introduce tu nombre: ");
         nombre = userInputVar.nextLine();
-        userInputVar.nextLine(); //Limpiar el buffer
         System.out.println("Te encuentras colgando de tu tela en el techo de la aula de 1º de DAM, que haces?");
         System.out.println("1: Bajas a explorar la clase");
         System.out.println("2: Subes tu tela para explorar el techo");
         decisionJuego = Main.depurarInput(userInputVar, 1, 2);
+        decionesTomadas ++;
         if(decisionJuego == 1){
             System.out.println("Cuidadosamente bajas al suelo de la clase. Una vez llegas al suelo ves el mundo desde " +
                     "una nueva perspectiva, ¿qué haces?");
@@ -27,6 +33,7 @@ public class Main{
             System.out.println("2: Ir a la mochila de un estudiante");
             System.out.println("3: Subir a la mesa del escritorio y esconderte en la chaqueta");
             decisionJuego = Main.depurarInput(userInputVar, 1, 3);
+            decionesTomadas ++;
             if(decisionJuego == 1){
                 //bajar -- cañería
                 System.out.println("Sin llamar la antención te subes a la ventana y observas que está abierta. " +
@@ -34,6 +41,7 @@ public class Main{
                 System.out.println("1: Bajar a fuera");
                 System.out.println("2: Subir la cañería");
                 decisionJuego = Main.depurarInput(userInputVar, 1, 2);
+                decionesTomadas ++;
                 if(decisionJuego == 1){
                     //muerte por balón
                     System.out.println("Disfrutas tu exploración del mundo exterior durante unos días, " +
@@ -45,6 +53,7 @@ public class Main{
                     System.out.println("Te cuesta escalar la cañería hasta que encuentras un rincón en el tejado " +
                             "que conviertes en una alcoba y tu nido");
                     System.out.println("HAPPY ENDING");
+                    happyEnding = true;
                     System.exit(0);
                 }
             }else if(decisionJuego == 2){
@@ -60,14 +69,17 @@ public class Main{
                 System.out.println("1: Decides bajar al suelo a explorar la clase");
                 System.out.println("2: Mejor esconderse, sube el muro al techo");
                 decisionJuego = Main.depurarInput(userInputVar, 1, 2);
+                decionesTomadas ++;
                 if(decisionJuego == 1){
                     System.out.println("Asustas a un estudiante en el proceso y te aplasta");
                     System.out.println("TRUE ENDING");
+                    trueEnding = true;
                     System.exit(0);
                 }else{
                     System.out.println("Intentas subir sin llamar la antención, pero rápidamente se escucha un " +
                             "grito y eres aplastado por un libro");
                     System.out.println("TRUE ENDING");
+                    trueEnding = true;
                     System.exit(0);
                 }
             }
@@ -78,6 +90,7 @@ public class Main{
             System.out.println("1: Vas a la derecha a explorar la oscuridad");
             System.out.println("2: Explorars en la dirección del ruído");
             decisionJuego = Main.depurarInput(userInputVar, 1, 2);
+            decionesTomadas ++;
             if(decisionJuego == 1){
                 //Rebelión de arañas
                 System.out.println("Te encuentras con un ejército de arañas esperándote. Una identificada con " +
@@ -86,6 +99,7 @@ public class Main{
                 System.out.println("Tras unos duros meses de conquista las arañas ahora son la " +
                         "especie dominante en la Tierra.");
                 System.out.println("DOMINACIÓN GLOBAL");
+                globalDomination = true;
                 System.exit(0);
             }else{
                 //Bajar a una nueva clase -- Girar a la izquierda
@@ -95,15 +109,32 @@ public class Main{
                 System.out.println("1: Bajas a la nueva clase");
                 System.out.println("2: Te diriges hacia el sonido");
                 decisionJuego = Main.depurarInput(userInputVar, 1, 2);
+                decionesTomadas ++;
                 if(decisionJuego == 1){
                     System.out.println("Bajas cuidadosamente del techo y asustas a un estudiante, " +
                             "eres rápidamente aplastado por un zapato.");
                     System.out.println("BAD ENDING COVARDE");
+                    cowardEnding = true;
                 }else{
                     System.out.println("De repente te encuentras en Hallownest y un bicho con un clavo te mata");
                     System.out.println("CROSSOVER ENDING");
+                    crossoverEnding = true;
                 }
             }
+        }
+        //Estadísticas de final de partida
+        System.out.println("ESTADÍSTICAS FINALES");
+        System.out.println("Decisiones tomadas: " + decionesTomadas);
+        if(happyEnding){
+            System.out.println("LOGRO OBTENIDO: Happy Ending C:");
+        }else if (crossoverEnding){
+            System.out.println("LOGRO OBTENIDO: Welcome to hallownest");
+        }else if(cowardEnding){
+            System.out.println("LOGRO NEGATIVO OBTENIDO: Covarde >:(");
+        } else if (trueEnding) {
+            System.out.println("LOGRO OBTENIDO: Triste Realidad :(");
+        }else if (globalDomination){
+            System.out.println("LOGRO OBTENIDO: Dominación Global >:)");
         }
     }
 }
